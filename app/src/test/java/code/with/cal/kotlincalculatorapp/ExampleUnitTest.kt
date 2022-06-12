76d7ee637ae2253c1bed6573d23984543f532c99
Alexandre Silva <alexandreasilva@protonmail.com>
2022-06-12 15:01:21 -0300

    Final exercise proposal Kotlin

---
+++ app/src/test/java/code/with/cal/kotlincalculatorapp/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package code.with.cal.kotlincalculatorapp
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
